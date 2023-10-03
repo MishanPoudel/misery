9ad50cd73e7888bb6c24654cd7cf1f3b601debd4
MishanPoudel <poudelmishan2@gmail.com>
2023-10-03 18:54:16 +0545

    fixed the game

--- replace.cpp
+++ replace.cpp
@@ -6,12 +6,13 @@ using namespace std;
 
 int main()
 {
-    bool zero = true;
     srand(time(0));
     int base[3][3];
     cout << "\t\tMatrix game" << endl;
     cout << "Objective:" << endl
          << "Try changing all 1s to 0s" << endl;
+
+    // Initialize the matrix
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -20,37 +21,66 @@ int main()
             base[i][j] = (randomValue < 7) ? 0 : 1;
         }
     }
+
+    // Print the initial matrix
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            cout << "\t\t" << base[i][j];
+            cout << "\t" << base[i][j];
         }
         cout << endl;
     }
 
-    int i, j, row, column;
+    int row, column;
+    bool allZeros = false;
+
     do
     {
-        cout << "Enter row:";
+        cout << "Enter row (0, 1, or 2): ";
         cin >> row;
-        cout << "Enter column:";
+        cout << "Enter column (0, 1, or 2): ";
         cin >> column;
-        cout << "Setting base[" << row << "][" << column << "] to 0" << endl;
-        base[row][column] = 0;
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                cout << base[i][j];
 
-                if (base[i][j] == 0)
+        // Input validation
+        if (row >= 0 && row < 3 && column >= 0 && column < 3)
+        {
+            cout << "Setting base[" << row << "][" << column << "] to 0" << endl;
+            base[row][column] = 0;
+
+            // Check if all values are now 0
+            allZeros = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
                 {
-                    zero = false;
-                    break;
+                    if (base[i][j] == 1)
+                    {
+                        allZeros = false;
+                        break;
+                    }
                 }
+                if (!allZeros)
+                    break;
             }
-            cout << endl;
+
+            // Print the updated matrix
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    cout << "\t" << base[i][j];
+                }
+                cout << endl;
+            }
         }
-    } while (zero = true);
+        else
+        {
+            cout << "Invalid input. Please enter row and column values between 0 and 2." << endl;
+        }
+    } while (!allZeros);
+
+    cout << "Congratulations! You've changed all 1s to 0s." << endl;
+
+    return 0;
 }
