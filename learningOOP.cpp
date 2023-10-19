ca5fa693baaae798da1f74cace87627c796d9c42
MishanPoudel <poudelmishan2@gmail.com>
2023-10-19 21:35:33 +0545

    day 2: Encapsulation

--- learningOOP.cpp
+++ learningOOP.cpp
@@ -1,36 +1,53 @@
 #include<iostream>
 using namespace std;
 
+class Employee {
+    private:
+        string name;
+        string company;
+        int age;
 
-class Employee{
     public:
-    string name;
-    string company;
-    int age;
+        void setName(string Name){ //getter
+            name = Name;
+        }
+        string getName(){  //setter
+            return name;
+        }
+        void setCompany(string Company){
+            company = Company;
+        }
+        string getCompany(){
+            return company;
+        }
+        void setAge(int Age){
+            if(Age >= 18)
+            age = Age;
+        }
+        int getAge(){
+            return age;
+        }
 
-    void IntroduceYourself(){
-        cout << "Name - " << name << endl;
-        cout << "Company - " << company << endl;
-        cout << "Age - " << age << endl;
-    }
+        void IntroduceYourself(){
+            cout << "Name - " << name << endl;
+            cout << "Company - " << company << endl;
+            cout << "Age - " << age << endl;
+        }
 
-    Employee(string Name, string Company, int Age){
-        name = Name;
-        company = Company;
-        age = Age; 
-    }
+        Employee(string Name, string Company, int Age){
+            name = Name;
+            company = Company;
+            age = Age; 
+        }
 };
 
-
 int main(){
-    Employee employee1= Employee("Misery", "Monke", 35), employee2 = Employee("Rizzery", "KAT", 35);
-    // employee1.name = "misery";
-    // employee1.company = "monke";
-    // employee1.age = 45;
+    Employee employee1 = Employee("Misery", "Monke", 35);
+    Employee employee2 = Employee("Rizzery", "KAT", 35);
+    
     employee1.IntroduceYourself();
-
-    // employee2.name = "rizzery";
-    // employee2.company = "KAT";
-    // employee2.age = 35;
     employee2.IntroduceYourself();
+
+    employee1.setAge(12);
+    cout << employee1.getName() << " is " << employee1.getAge() << " years old" << endl;
 }
