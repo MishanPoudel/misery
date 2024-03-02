df30c1e0dfd255cc2a97afe8560e8265b3ef15da
Mishan Poudel <poudelmishan2@gmail.com>
2024-03-02 13:20:35 +0000

    yes

---
+++ big brain/index.js
@@ -0,0 +1,14 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment')
+const simpleGit = require('simple-git')
+const FILE_PATH = './data.json';
+// for day before
+const DATE = moment().subtract(1, 'd').format()
+
+const data = {
+    data: DATE
+}
+
+jsonfile.writeFile(FILE_PATH, data, ()=>{
+    simpleGit().add([FILE_PATH]).commit("yes", {'--date': DATE }).push();
+})
